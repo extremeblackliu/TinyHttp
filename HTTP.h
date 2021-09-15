31a34ed52acb01f096c8d49e02305ff4fdd878e8
ExtremeBlackLiu <54531236+extremeblackliu@users.noreply.github.com>
2021-09-15 20:24:53 +0800

    Create HTTP.h

---
+++ HTTP.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class HttpRequest
+{
+public:
+    HttpRequest(const std::string& ip, int port);
+    
+    std::string HttpGet(std::string req);
+
+    std::string HttpPost(std::string req, std::string data);
+
+    static std::string genJsonString(std::string key, int value);
+
+    static std::vector<std::string> split(const std::string& s, const std::string& seperator);
+
+    static std::string getHeader(std::string respose, std::string key);
+
+private:
+    std::string         m_ip;
+    int             m_port;
+};
